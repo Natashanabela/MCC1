bda27b483d80a37bdf8396b883acb2c15f74fdc1
natashanabela <natashaanabela17@gmail.com>
2023-09-15 10:29:00 +0700

    first commit

---
+++ bin/tutor_dasar.dart
@@ -0,0 +1,5 @@
+import 'package:tutor_dasar/tutor_dasar.dart' as tutor_dasar;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tutor_dasar.calculate()}!');
+}
---
+++ test/tutor_dasar_test.dart
@@ -0,0 +1,8 @@
+import 'package:tutor_dasar/tutor_dasar.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
